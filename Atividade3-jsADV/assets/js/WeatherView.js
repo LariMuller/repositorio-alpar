82d815e7fd5d372f82ce5f925ce0c186f24f7903
Larissa Muller <lariitcmuller@gmail.com>
2024-04-21 23:43:45 -0300

    add css atv 3

--- Atividade3-jsADV/assets/js/WeatherView.js
+++ Atividade3-jsADV/assets/js/WeatherView.js
@@ -3,13 +3,15 @@ const tempEl = document.querySelector(".temp")
 const descriptionEl = document.querySelector(".description")
 const tempMinEl = document.querySelector(".min")
 const tempMaxEl = document.querySelector(".max")
+const cardEl = document.querySelector(".card")
 
 class WeatherView{
     changeCard(response){
+        cardEl.classList.add("card-show")
         cityEl.innerHTML = `${response.name}`
         tempEl.innerHTML = `${response.main.temp.toFixed()}ºC`
-        descriptionEl.innerHTML = `${response.weather[0].description.toUpperCase()}`
-        tempMinEl.innerHTML = `${response.main.temp_min.toFixed()}ºC`
-        tempMaxEl.innerHTML = `${response.main.temp_max.toFixed()}ºC`
+        descriptionEl.innerHTML = `Descrição: ${response.weather[0].description.toUpperCase()}`
+        tempMinEl.innerHTML = `Min: ${response.main.temp_min.toFixed()}ºC`
+        tempMaxEl.innerHTML = `Max: ${response.main.temp_max.toFixed()}ºC`
     }
 }
